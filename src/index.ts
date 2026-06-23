40bd4afbb23dd71454155046eac57b9d46aeff7f
psmacabulos <patrick.macabulos@gmail.com>
2026-06-23 23:45:57 +1000

    feat: add POST /workout_sessions and GET /workout_sessions/me

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import 'dotenv/config';
 import express from 'express';
 import authRouter from './routes/auth.routes';
 import exerciseRouter from './routes/exercise.routes';
+import workoutRouter from './routes/workout.routes';
 import cors from 'cors';
 import helmet from 'helmet';
 import { connectDB } from './config/db';
@@ -15,6 +16,7 @@ app.use(cors());
 app.use(express.json());
 app.use('/v1/auth', authRouter);
 app.use('/v1/exercises', exerciseRouter);
+app.use('/v1/workout_sessions', workoutRouter);
 
 app.get('/health', (req, res) => {
   res.json({ status: 'ok' });
